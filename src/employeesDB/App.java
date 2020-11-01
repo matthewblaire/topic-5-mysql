756de0e6736f5308e2e1e4c533d4695dfb846410
matthewblaire <49771927+matthewblaire@users.noreply.github.com>
2020-10-31 18:09:38 -0700

    Update App.java

--- src/employeesDB/App.java
+++ src/employeesDB/App.java
@@ -1,13 +1,11 @@
 package employeesDB;
 
-import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Types;
 import java.util.Scanner;
 public class App {
 	private static Connection conn;
